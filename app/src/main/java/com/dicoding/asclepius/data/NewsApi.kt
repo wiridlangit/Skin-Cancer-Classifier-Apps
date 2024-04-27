7ca05a5e73ba0dcb2e88e14f72ed9c5684444349
wiridlangit <113527799+wiridlangit@users.noreply.github.com>
2024-04-27 20:23:52 +0700

    Update NewsApi.kt

--- app/src/main/java/com/dicoding/asclepius/data/NewsApi.kt
+++ app/src/main/java/com/dicoding/asclepius/data/NewsApi.kt
@@ -4,7 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface NewsApi {
-    @GET("v2/top-headlines?q=cancer&category=health&language=en&apiKey=77203d03338942bd8d3fcfe477f8bb00")
+    @GET("v2/top-headlines?q=cancer&category=health&language=en&apiKey={YOUR_API_TOKEN}")
     fun getNews(): Call<NewsResponse>
 }
 
